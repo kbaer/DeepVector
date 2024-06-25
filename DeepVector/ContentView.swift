6c39e5c47504f24413fe5cab4cda4496e601257f
Ken Baer <kenbaer@me.com>
2024-06-25 12:13:10 -0700

    Got sequential animation working with animate toggle

--- DeepVector/ContentView.swift
+++ DeepVector/ContentView.swift
@@ -20,21 +20,6 @@ struct Triangle: Shape {
         
         return path
     }
-    
-    func animateShape(index: Int, isAnimated: Bool, deltaAngle: Double) -> some View {
-        let twoPi: Double = 2.0 * Double.pi
-        let deltaFactor = sin(Double(index)/15.0 * twoPi)
-        let rotateDelta = isAnimated ? deltaAngle : -deltaAngle * 2
-        let scaleDelta = isAnimated ? (0.1 * deltaFactor) : 0.0
-        return self
-            .stroke(.green, style: StrokeStyle(lineWidth: 8
-                                             , lineCap: .round, lineJoin: .round))
-            .frame(width: 800, height: 800, alignment: .bottom)
-            .offset(z: CGFloat(index * 20))
-            .rotationEffect(Angle(degrees: CGFloat(Double(index) * rotateDelta)))
-            .scaleEffect(CGSize(width: 0.6 + scaleDelta, height: 0.6 + scaleDelta))
-            .animation(.linear(duration: 1.0).delay(Double(index)*0.1).repeatCount(4, autoreverses: true), value: isAnimated)
-    }
 }
 
 struct TriangleModel: Identifiable {
@@ -42,22 +27,47 @@ struct TriangleModel: Identifiable {
     var scale: Double
     var rotation: Double
     var color: UIColor
+    var triangleShape: Triangle
 }
 
 struct IterationView: View {
     @Binding var deltaAngle: Double
-    @Binding var isAnimated: Bool
-    @Binding var throbAmount: Double
-    var triangleArray: [TriangleModel] = Array(repeating: TriangleModel(scale: 1.0, rotation: 0.0, color: .blue), count: 40)
+    @Binding var animate: Bool
+    @Binding var spacing: Double
+    @State var rotationAnimationTrigger = false
+    var triangleArray: [TriangleModel] = Array(repeating: TriangleModel(scale: 1.0, rotation: 0.0, color: .blue, triangleShape: Triangle()), count: 50)
     
     @ViewBuilder
     var body: some View {
-        
+//        let twoPi: Double = 2.0 * Double.pi
         ZStack {
-            ForEach(Array(triangleArray.enumerated()), id: \.offset) { index, triangle in
-                Triangle()
-                    .animateShape(index: index, isAnimated: isAnimated, deltaAngle: deltaAngle)
+//            let triangle = triangleArray[0]
+//            let index = 0
+           ForEach(Array(triangleArray.enumerated()), id: \.offset) { index, triangle in
+//                let deltaFactor = sin(Double(index)/15.0 * twoPi)
+                let rotateDelta = animate ? deltaAngle : -deltaAngle
+                triangle.triangleShape
+                    .stroke(.green, style: StrokeStyle(lineWidth: 8
+                                                     , lineCap: .round, lineJoin: .round))
+                    .frame(width: 800, height: 800, alignment: .bottom)
+                    .offset(z: CGFloat(Double(index) * spacing))
+                    .rotationEffect(Angle(degrees: animate ? (CGFloat(Double(index) * ( rotationAnimationTrigger ? rotateDelta : -rotateDelta))) :  Double(index) * rotateDelta))
+                    .scaleEffect(CGSize(width: 0.6, height: 0.6))
+                    .id(animate)
+                    .animation(.easeInOut(duration: 1.0).delay(Double(index)*0.1).repeatForever(autoreverses: true), value: rotationAnimationTrigger)
             }
+        }
+        .onChange(of: animate){ oldValue, shouldChange in
+            let animation = Animation.linear(duration: 1.0)
+            if shouldChange {
+                withAnimation(animation) {
+                    rotationAnimationTrigger = true
+                }
+            } else {
+                rotationAnimationTrigger = false
+            }
+        }
+        .onAppear() {
         }
     }
 }
@@ -65,32 +75,44 @@ struct IterationView: View {
 struct ContentView: View {
     
     @State var deltaAngle : Double = 3.0
-    @State var isAnimated: Bool = false
-    @State var throbAmount: Double = 0.0
+    @State var animate: Bool = false
+    @State var spacing: Double = 20
 
     var body: some View {
         HStack {
-            var iterationView = IterationView(deltaAngle: $deltaAngle, isAnimated: $isAnimated, throbAmount: $throbAmount)
+            let iterationView = IterationView(deltaAngle: $deltaAngle, animate: $animate, spacing: $spacing)
             iterationView
             VStack (spacing: 12) {
-                Text("Delta Degrees:")
                 Text(
-                    String(format: "%.1f", deltaAngle)
+                    String(format: "Delta Degrees: %.1f", deltaAngle)
                 )
                 Slider(value: $deltaAngle,
-                       in: 0...15,
+                       in: 0...10,
                        onEditingChanged: { (_) in
-                    iterationView.deltaAngle = deltaAngle
+                            iterationView.deltaAngle = deltaAngle
+                       },
+                       minimumValueLabel: Text("0"),
+                       maximumValueLabel: Text("10"),
+                       label: {
+                           Text("Title")
+                       })
+                Text(
+                    String(format: "Spacing: %.1f", spacing)
+                )
+                Slider(value: $spacing,
+                       in: 0...50,
+                       onEditingChanged: { (_) in
+                    iterationView.spacing = spacing
                 },
                        minimumValueLabel: Text("0"),
-                       maximumValueLabel: Text("15"),
+                       maximumValueLabel: Text("50"),
                        label: {
                     Text("Title")
                 })
-                .accentColor(.red)
                 Spacer()
-                Toggle("Animate", isOn: $isAnimated//.animation(.easeInOut.repeatForever(autoreverses: true))
-                )
+                Button("\(iterationView.animate ? "Stop" : "Start") Animation") { // add this button
+                    animate.toggle()
+                }
              }
             .frame(width: 400, height: 300)
             .padding(36)
